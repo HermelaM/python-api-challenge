df7908e61dbbc6c3e70c36029f2a218bfc67dd4a
HermelaM <hermela.bb@gmail.com>
2020-08-05 00:41:05 -0700

    HW

---
+++ VaccationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = a55b6fad7716c824963577da3a08efcc
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
---
+++ WeattherPy/api_keys.py
@@ -0,0 +1,6 @@
+
+# OpenWeatherMap API Key
+weather_api_key = "a55b6fad7716c824963577da3a08efcc"
+
+# Google API Key
+g_key = "AIzaSyDhAtBPwzpLYDAutRFB0F-YtSEmTh3HReI"
